6807d28163e82b2e378f58a6c8be9d85cedbcf05
Ricarte Venerayan <venerayan@gmail.com>
2018-10-11 10:24:46 -0400

    Create sshcopy.sh

---
+++ sshcopy.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+rpm -qa sshpass > epelx
+if [[ ! -s epelx ]]; then
+   sudo yum install sshpass -y
+fi
+
+if [ ! -f ~/.ssh/id_rsa ]; then
+	ssh-keygen -q -f ~/.ssh/id_rsa -N ""
+fi
+
+if [ ! -f secret ]; then
+   echo
+   echo "Please create your secret first! (e.g. echo Your_password > secret)"
+   exit 1
+fi
+
+pass=$(cat secret)
+awk -F' ' 'FNR > 2 { print  $2 }' /etc/hosts > sshcopy
+awk NF sshcopy > linux
+awk -v password="$pass" '{print "sshpass -p " password " ssh-copy-id -o StrictHostKeyChecking=no " $1}' linux > sshcopy
+chmod u+x sshcopy
+./sshcopy
+
+rm -f linux copy sshcopy
